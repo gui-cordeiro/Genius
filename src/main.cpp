30ab199b033cc7262017cb649d3bfad1737a3503
gui-cordeiro <guisg.cordeiro@gmail.com>
2024-02-23 11:51:47 -0300

    adicionado últimos ajustes do projeto

--- src/main.cpp
+++ src/main.cpp
@@ -12,23 +12,28 @@
 #include <Arduino.h>
 
 // Declaração dos pinos digitais para os LED's, pushbuttons e buzzer (configurado para o Arduino UNO):
-const int LED_Amarelo = 13;
-const int LED_Verde = 12;
-const int LED_Vermelho = 11;
-const int LED_Azul = 10;
-const int btn_Amarelo = 5;
-const int btn_Verde = 4;
-const int btn_Vermelho = 3;
-const int btn_Azul = 2;
-const int Buzzer = 9;
+const int LED1 = 13;
+const int LED2 = 12;
+const int LED3 = 11;
+const int LED4 = 10;
+const int btn1 = 5;
+const int btn2 = 4;
+const int btn3 = 3;
+const int btn4 = 2;
+const int buzzer = 9;
 
 // Definição das frequências sonoras relativas a cada cor
-const int Nota1 = 294;
-const int Nota2 = 392;
-const int Nota3 = 440;
-const int Nota4 = 880;
+const int freq1 = 294;
+const int freq2 = 392;
+const int freq3 = 440;
+const int freq4 = 880;
 
-// Vetor que armazena a sequência de cores do jogo
+/* Vetor que armazena a sequência de 100 cores do jogo.
+ *
+ * A ideia de criar um vetor dinâmico (e consequentemente criar uma sequência de cores indefinida)
+ * foi descartada por questões de limitação de memória dos sistemas embarcados, pois poderiam causar 
+ * problemas inesperados na execução do programa.
+ */
 int sequencia[100] = {};
 
 // Variáveis de controle
@@ -36,22 +41,23 @@ int btnPressionado = -1;
 int controle = 0;
 int cont = 0;
 
-// Declaração da função auxiliar
+// Declaração das funções auxiliares
 void acenderLED(int LED, int freq, int delayLED);
+void zerarVetor();
 
 void setup() {
    Serial.begin(9600);
    
-   // Setando os pinos como "Entrada(Pull-Up)" ou "Saída"
-   pinMode(LED_Amarelo, OUTPUT);
-   pinMode(LED_Verde, OUTPUT);
-   pinMode(LED_Vermelho, OUTPUT);
-   pinMode(LED_Azul, OUTPUT);
-   pinMode(btn_Amarelo, INPUT_PULLUP);
-   pinMode(btn_Verde, INPUT_PULLUP);
-   pinMode(btn_Vermelho, INPUT_PULLUP);
-   pinMode(btn_Azul, INPUT_PULLUP);
-   pinMode(Buzzer, OUTPUT);
+   // Define o modo dos pinos
+   pinMode(LED1, OUTPUT);
+   pinMode(LED2, OUTPUT);
+   pinMode(LED3, OUTPUT);
+   pinMode(LED4, OUTPUT);
+   pinMode(btn1, INPUT_PULLUP);
+   pinMode(btn2, INPUT_PULLUP);
+   pinMode(btn3, INPUT_PULLUP);
+   pinMode(btn4, INPUT_PULLUP);
+   pinMode(buzzer, OUTPUT);
 
    // Mensagem de boas-vindas
    Serial.println("\n\n+--------------------------+");
@@ -62,36 +68,36 @@ void setup() {
    Serial.println("| errar uma cor!           |");
    Serial.println("+--------------------------+\n\n");
 
-   // Gerando uma seed para criar uma sequência aleatória de cores
+   // Gera uma seed para criar uma sequência aleatória de cores
    randomSeed(analogRead(A0));
    
    // Rotina de introdução ao jogo
    for (int cont = 0; cont < 2; cont ++) {
-      acenderLED(LED_Amarelo, 0, 150);
-      acenderLED(LED_Verde, 0, 150);
-      acenderLED(LED_Vermelho, 0, 150);
-      acenderLED(LED_Azul, 0, 150);
-      acenderLED(LED_Vermelho, 0, 150);
-      acenderLED(LED_Verde, 0, 150);
+      acenderLED(LED1, 0, 150);
+      acenderLED(LED2, 0, 150);
+      acenderLED(LED3, 0, 150);
+      acenderLED(LED4, 0, 150);
+      acenderLED(LED3, 0, 150);
+      acenderLED(LED2, 0, 150);
    }
-   acenderLED(LED_Amarelo, 0, 150);
+   acenderLED(LED1, 0, 150);
    for (int cont = 0; cont <= 3; cont ++) {
-      digitalWrite(LED_Amarelo, HIGH);
-      digitalWrite(LED_Verde, HIGH);
-      digitalWrite(LED_Vermelho, HIGH);
-      digitalWrite(LED_Azul, HIGH);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, HIGH);
       delay(100);
-      digitalWrite(LED_Amarelo, LOW);
-      digitalWrite(LED_Verde, LOW);
-      digitalWrite(LED_Vermelho, LOW);
-      digitalWrite(LED_Azul, LOW);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, LOW);
       delay(100);
    }
    delay(1400);
 }
 
 void loop() {
-   // O jogo atualmente armazena uma sequência de 100 cores. Isso será alterado futuramente para que a sequência seja indefinida.
+   // O jogo armazena uma sequência de 100 cores (mais explicações na linha 31)
    for(controle = 0; controle < 100; controle ++) {
 
       // Mensagem de inicio de jogo
@@ -100,31 +106,31 @@ void loop() {
       // Escolhe qual LED será aceso e armazena no vetor
       sequencia[controle] = random(4);
 
-      // Loop que percorrerá por todo o vetor de sequência e acenderá as respectivas cores
+      // Loop que percorre por todo o vetor de sequência e acenderá as respectivas cores
       for (int numero = 0; numero <= controle; numero ++) {
-         if (sequencia[numero] == 0) acenderLED(LED_Amarelo, Nota1, 500);
-         else if (sequencia[numero] == 1) acenderLED(LED_Verde, Nota2, 500);
-         else if (sequencia[numero] == 2) acenderLED(LED_Vermelho, Nota3, 500);
-         else if (sequencia[numero] == 3) acenderLED(LED_Azul, Nota4, 500);
+         if (sequencia[numero] == 0) acenderLED(LED1, freq1, 500);
+         else if (sequencia[numero] == 1) acenderLED(LED2, freq2, 500);
+         else if (sequencia[numero] == 2) acenderLED(LED3, freq3, 500);
+         else if (sequencia[numero] == 3) acenderLED(LED4, freq4, 500);
          delay(100);
       }
 
-      // Loop que aguardará a ação do jogador. Ao pressionar um botão, será verificado se tal botão corresponde à cor correta.
+      // Loop que aguarda a ação do jogador. Ao pressionar um botão, será verificado se tal botão corresponde à cor correta.
       for (int numero = 0; numero <= controle; numero ++) {
          btnPressionado = -1;
          while (btnPressionado == -1) {
-            if (digitalRead(btn_Amarelo) == 0) {
+            if (digitalRead(btn1) == 0) {
                btnPressionado = 0;
-               acenderLED(LED_Amarelo, Nota1, 300);
-            } else if (digitalRead(btn_Verde) == 0) {
+               acenderLED(LED1, freq1, 300);
+            } else if (digitalRead(btn2) == 0) {
                btnPressionado = 1;
-               acenderLED(LED_Verde, Nota2, 300);
-            } else if (digitalRead(btn_Vermelho) == 0) {
+               acenderLED(LED2, freq2, 300);
+            } else if (digitalRead(btn3) == 0) {
                btnPressionado = 2;
-               acenderLED(LED_Vermelho, Nota3, 300);
-            } else if (digitalRead(btn_Azul) == 0) {
+               acenderLED(LED3, freq3, 300);
+            } else if (digitalRead(btn4) == 0) {
                btnPressionado = 3;
-               acenderLED(LED_Azul, Nota4, 300);
+               acenderLED(LED4, freq4, 300);
             }
          }
          delay(10);
@@ -132,7 +138,7 @@ void loop() {
          // Se "verdadeiro": Botão errado, fim de jogo! Se falso: Botão correto, o jogo continua!
          if (btnPressionado != sequencia[numero]) {
 
-            // Printando a pontuação final e a resetando em seguida
+            // Printa a pontuação final e a reseta em seguida
             Serial.print("-> FIM DE JOGO! Pontuação final: ");
             Serial.print(controle);
             Serial.println("\n\n");
@@ -140,29 +146,27 @@ void loop() {
 
             // Rotina de encerramento do jogo
             delay(50);
-            acenderLED(LED_Amarelo, Nota4, 180);
-            acenderLED(LED_Verde, Nota3, 180);
-            acenderLED(LED_Vermelho, Nota2, 180);
-            acenderLED(LED_Azul, Nota1, 180);
-            tone(Buzzer, Nota1);
+            acenderLED(LED1, freq4, 180);
+            acenderLED(LED2, freq3, 180);
+            acenderLED(LED3, freq2, 180);
+            acenderLED(LED4, freq1, 180);
+            tone(buzzer, freq1);
             for (int cont = 0; cont < 4; cont ++) {
-               digitalWrite(LED_Amarelo, HIGH);
-               digitalWrite(LED_Verde, HIGH);
-               digitalWrite(LED_Vermelho, HIGH);
-               digitalWrite(LED_Azul, HIGH);
+               digitalWrite(LED1, HIGH);
+               digitalWrite(LED2, HIGH);
+               digitalWrite(LED3, HIGH);
+               digitalWrite(LED4, HIGH);
                delay(100);
-               digitalWrite(LED_Amarelo, LOW);
-               digitalWrite(LED_Verde, LOW);
-               digitalWrite(LED_Vermelho, LOW);
-               digitalWrite(LED_Azul, LOW);
+               digitalWrite(LED1, LOW);
+               digitalWrite(LED2, LOW);
+               digitalWrite(LED3, LOW);
+               digitalWrite(LED4, LOW);
                delay(100);
             }
-            noTone(Buzzer);
+            noTone(buzzer);
 
-            // Limpando vetor para iniciar um novo jogo
-            for (int limpar = 0; limpar < 100; limpar ++) {
-               sequencia[limpar] = -1;
-            }
+            // Limpa o vetor para iniciar um novo jogo
+            zerarVetor();
 
             delay(1000);
             break;
@@ -172,13 +176,26 @@ void loop() {
       // Pequeno delay para iniciar um novo loop e prosseguir com uma nova cor
       delay(1000);
    }
+   Serial.println("\n\nVocê chegou ao fim do jogo. O sketch está limitado a armazenar um limite de 100 cores devido a questões de limitação de memória dos sistemas embarcados.\n");
+   Serial.println("O jogo será reiniciado em instantes...\n\n");
+
+   // Limpa o vetor para iniciar um novo jogo
+   zerarVetor();
+   delay(5000);
 }
 
 // A) Função para acender um determinado LED e acionar o buzzer numa frequência determinada, por um breve período de tempo (em ms)
 void acenderLED(int LED, int freq, int delayLED) {
    digitalWrite(LED, HIGH);
-   if (freq != 0) tone(Buzzer, freq);
+   if (freq != 0) tone(buzzer, freq);
    delay(delayLED);
    digitalWrite(LED, LOW);
-   noTone(Buzzer);
+   noTone(buzzer);
+}
+
+// B) Função que limpa o vetor de sequência de cores
+void zerarVetor() {
+   for (int limpar = 0; limpar < 100; limpar ++) {
+      sequencia[limpar] = -1;
+   }
 }
